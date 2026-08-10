6ea2721180ec69edd8a163ecfef80b7754e6b82d
busrakuscu0 <busrakuscu6991@gmail.com>
2026-08-10 02:37:30 +0200

    fix: correct roles claim URL in Auth0 configuration

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 export const auth0 = new Auth0Client();
 
-const ROLES_CLAIM = "https://ecom-200825";
+const ROLES_CLAIM = "https://ecom-200825/roles";
 
 export enum AppRole {
   USER = "user",
